8fe2506c37a318680bc28716a6b9787e2d6a9987
Glauco Lustosa <glauco.lustosa@icloud.com>
2022-04-12 11:03:38 -0300

    ReadMe

--- movie-selector/src/components/MovieList.js
+++ movie-selector/src/components/MovieList.js
@@ -5,7 +5,7 @@ const MovieList = (props) => {
     return (
         <>
             {props.movies.map((movie, index) => 
-            <div className='image-container d-flex movie-frame justify-content-start p-0 m-3'>
+            <div key={index} className='image-container d-flex movie-frame justify-content-start p-0 m-3'>
                 <img src={movie.Poster} alt='movie'></img>
                 <div 
                 onClick={() => props.handleFavouriteClick(movie)}
